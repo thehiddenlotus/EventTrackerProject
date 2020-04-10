6ff41bd921266f6b5a1563d8498ef8ef6f79915e
Luke Sutherland <luke.sutherland@live.com>
2020-04-10 16:29:17 -0600

    created rest project

---
+++ GratefulREST/src/main/java/com/skilldistillery/grateful/GratefulRestApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.grateful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratefulRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratefulRestApplication.class, args);
+	}
+
+}
---
+++ GratefulREST/src/test/java/com/skilldistillery/grateful/GratefulRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.grateful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratefulRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

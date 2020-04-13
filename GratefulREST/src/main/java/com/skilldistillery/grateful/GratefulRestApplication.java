69f5ae8add3d3c66b0cffcfec0251b6abbabedfb
Luke Sutherland <luke.sutherland@live.com>
2020-04-13 08:40:57 -0600

    created war file

--- GratefulREST/src/main/java/com/skilldistillery/grateful/GratefulRestApplication.java
+++ GratefulREST/src/main/java/com/skilldistillery/grateful/GratefulRestApplication.java
@@ -2,12 +2,19 @@ package com.skilldistillery.grateful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GratefulRestApplication {
+public class GratefulRestApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GratefulRestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GratefulRestApplication.class, args);
+
 	}
 
 }

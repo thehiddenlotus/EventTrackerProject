d8d9745f98cef9c22a6320f29a206b9fe35b21e2
Luke Sutherland <luke.sutherland@live.com>
2020-04-17 18:49:00 -0600

    need to commit more, everything working besides edit

--- GratefulREST/src/main/resources/static/js/script.js
+++ GratefulREST/src/main/resources/static/js/script.js
@@ -5,5 +5,171 @@ window.addEventListener('load', evt => {
 
 function init() {
 	console.log('In init');
+	document.searchForm.lookup.addEventListener('click', function(e){
+		e.preventDefault();
+		let entryId = document.searchForm.entryId.value;
+		console.log(entryId);
+		if (!isNaN(entryId) && entryId > 0) {
+			getEntry(entryId);
+		}
+	});
+	document.entryForm.add.addEventListener('click',function(e){
+		e.preventDefault();
+		createEntry(document.entryForm);
+	});
+}
+
+function getEntry(entryId) {
+	console.log('in getEntry');
 	
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/entries/' + entryId);
+	xhr.onreadystatechange = function() {
+		if (xhr.status < 400 && xhr.readyState === 4) {
+			var data = JSON.parse(xhr.responseText);
+			console.log(data);
+			displayEntry(data);
+		} else if (xhr.readyState === 4 && xhr.status >= 400) {
+			console.error('ERROR');
+		}
+	}
+	xhr.send();
+}
+
+function displayEntry(entry) {
+	var dataDiv = document.getElementById('entryDiv');
+	dataDiv.textContent = '';
+	
+	if(entry.date != undefined){
+		let title = document.createElement('h2');
+		let date = document.createElement('h3');
+		let mood = document.createElement('h4');
+		let gratitude = document.createElement('blockquote');
+		let note = document.createElement('blockquote');
+		
+		title.textContent = entry.id + '    ' + entry.title;
+		date.textContent = entry.date;
+		mood.textContent = entry.mood;
+		gratitude.textContent = entry.gratitude;
+		note.textContent = entry.note;
+		
+		dataDiv.appendChild(title);
+		dataDiv.appendChild(date);
+		dataDiv.appendChild(mood);
+		dataDiv.appendChild(gratitude);
+		dataDiv.appendChild(note);
+		
+		let edit = document.createElement('button');
+		let remove = document.createElement('button');
+		edit.textContent = 'Edit Entry';
+		remove.textContent = 'Remove Entry';
+		
+		edit.addEventListener('click', function(e){
+			e.preventDefault();
+			let id = entry.id;
+			console.log(id);
+			if (!isNaN(id) && id > 0) {
+				editEntry(id);
+			}
+		});
+		
+		remove.addEventListener('click', function(e){
+			e.preventDefault();
+			let id = entry.id;
+			console.log(id);
+			if (!isNaN(id) && id > 0) {
+				removeEntry(id);
+			}
+		});
+		
+		dataDiv.appendChild(edit);
+		dataDiv.appendChild(remove);
+	}
+	else{
+		// let noEntry = document.createElement('h4');
+		// noEntry.textContent = 'No Entry';
+		// dataDiv.appendChild(noEntry);
+		document.entryDiv.textContent = 'No Entry';
+	}
+}
+	
+function createEntry(formObj){
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/entries');
+	xhr.setRequestHeader("Content-type", "application/json"); 
+	xhr.onreadystatechange = function() {
+	  if (xhr.readyState === 4 ) {
+	    if ( xhr.status == 200 || xhr.status == 201 ) { 
+	      var data = JSON.parse(xhr.responseText);
+	      console.log(data);
+		  displayEntry(data);
+		  document.entryForm.reset();
+	    }
+	    else {
+	      console.log("POST request failed.");
+	      console.error(xhr.status + ': ' + xhr.responseText);
+	    }
+	  }
+	};
+	var entryObject = {
+			  title: formObj.title.value,
+			  date: formObj.date.value,
+			  mood: formObj.mood.value,
+			  gratitude: formObj.gratitude.value,
+			  note: formObj.note.value  
+			};
+	var entryObjectJson = JSON.stringify(entryObject); 
+	xhr.send(entryObjectJson);
+}
+
+function removeEntry(entryId){
+	var xhr = new XMLHttpRequest();
+	xhr.open('DELETE', 'api/entries/'+entryId);
+	xhr.setRequestHeader("Content-type", "application/json"); 
+	xhr.onreadystatechange = function() {
+	  if (xhr.readyState === 4 ) {
+	    if ( xhr.status < 400 ) { 
+	      var data = JSON.parse(xhr.responseText);
+	      console.log(data);
+		  document.entryDiv.innerHTML = '';
+		  document.entryDiv.textContent = 'Entry Removed';
+	    }
+	    else {
+	      console.log("DELETE request failed.");
+	      console.error(xhr.status + ': ' + xhr.responseText);
+	    }
+	  }
+	};
+	
+	xhr.send(null);
+}
+
+// JUST A TEMPLATE STILL NEEDS DONE !!!
+function editEntry(formObj){
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/entries');
+	xhr.setRequestHeader("Content-type", "application/json"); 
+	xhr.onreadystatechange = function() {
+	  if (xhr.readyState === 4 ) {
+	    if ( xhr.status == 200 || xhr.status == 201 ) { 
+	      var data = JSON.parse(xhr.responseText);
+	      console.log(data);
+		  displayEntry(data);
+		  document.entryForm.reset();
+	    }
+	    else {
+	      console.log("POST request failed.");
+	      console.error(xhr.status + ': ' + xhr.responseText);
+	    }
+	  }
+	};
+	var entryObject = {
+			  title: formObj.title.value,
+			  date: formObj.date.value,
+			  mood: formObj.mood.value,
+			  gratitude: formObj.gratitude.value,
+			  note: formObj.note.value  
+			};
+	var entryObjectJson = JSON.stringify(entryObject); 
+	xhr.send(entryObjectJson);
 }
